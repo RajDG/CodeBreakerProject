785667251c9880fb31e81e2594d0c0c72d44ccf2
RajDG <Rajesh_Ghodasara@yahoo.com>
2017-11-20 15:48:05 -0500

    Added value for fields

--- assets/main.js
+++ assets/main.js
@@ -5,18 +5,18 @@ function guess() {
     let input = document.getElementById('user-guess');
 
     //add functionality to guess function here
-    if (answer == '' || attempt == '') {
+    if (answer.value == '' || attempt.value == '') {
       setHiddenFields();
     }
 }
 
 //implement new functions here
 function setHiddenFields() {
-  answer = Math.floor(Math.random() * 10000).toString();
+  answer.value = Math.floor(Math.random() * 10000).toString();
 
-  while(answer.length < 4) {
-    answer = "0" + answer;
+  while(answer.value.length < 4) {
+    answer.value = "0" + answer.value;
   }
 
-  attempt = "0";
+  attempt.value = "0";
 }
