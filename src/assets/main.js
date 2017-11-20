6ae71bc8afb766b0e39c0671a9e1a79ba3e74990
RajDG <Rajesh_Ghodasara@yahoo.com>
2017-11-20 15:26:45 -0500

    Added setHiddenFields method

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,7 +3,21 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
+
     //add functionality to guess function here
+    if (answer == '' || attempt == '') {
+      setHiddenFields();
+    }
 }
 
 //implement new functions here
+function setHiddenFields() {
+  answer = Math.random();
+  answer = Math.floor(answer * 10000).toString();
+
+  while(answer.length < 4) {
+    answer = "0" + answer;
+  }
+
+  attempt = "0";
+}
